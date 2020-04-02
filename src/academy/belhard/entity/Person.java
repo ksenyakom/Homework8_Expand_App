456f7abb1bbb039f74d090b12ea01e3acf57da4b
ksenys kom <ksenyakom@gmail.com>
2020-04-02 11:01:55 +0300

    first commit

---
+++ src/academy/belhard/entity/Person.java
@@ -0,0 +1,78 @@
+package academy.belhard.entity;
+
+import java.util.Objects;
+
+/**
+ * Просто класс инкапсулирующий данные о некотором человеке
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+
+    public Person(String firstName, String lastName, String email, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                firstName.equals(person.firstName) &&
+                lastName.equals(person.lastName) &&
+                email.equals(person.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, email, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
